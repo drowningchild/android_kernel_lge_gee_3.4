8b91498dcdaefee0ed2dc7db9037df98136de8ee
Sameer Thalappil <sameert@codeaurora.org>
2012-12-24 08:59:21 -0800

    wcnss: Use preferred devm_kzalloc
    Replace kzmalloc with preferred devm_kzalloc to avoid explicit memory
    free calls.
    
    Change-Id: I5457bad7e9ecab36e9d9fe9d429b121669d2f9d3
    CRs-Fixed: 421013
    Signed-off-by: Sameer Thalappil <sameert@codeaurora.org>

--- drivers/net/wireless/wcnss/wcnss_wlan.c
+++ drivers/net/wireless/wcnss/wcnss_wlan.c
@@ -775,7 +775,6 @@ fail_power:
 	else
 		wcnss_gpios_config(penv->gpios_5wire, false);
 fail_gpio_res:
-	kfree(penv);
 	penv = NULL;
 	return ret;
 }
@@ -816,7 +815,7 @@ wcnss_wlan_probe(struct platform_device *pdev)
 	}
 
 	/* create an environment to track the device */
-	penv = kzalloc(sizeof(*penv), GFP_KERNEL);
+	penv = devm_kzalloc(&pdev->dev, sizeof(*penv), GFP_KERNEL);
 	if (!penv) {
 		dev_err(&pdev->dev, "cannot allocate device memory.\n");
 		return -ENOMEM;
@@ -825,8 +824,10 @@ wcnss_wlan_probe(struct platform_device *pdev)
 
 	/* register sysfs entries */
 	ret = wcnss_create_sysfs(&pdev->dev);
-	if (ret)
+	if (ret) {
+		penv = NULL;
 		return -ENOENT;
+	}
 
 
 #ifdef MODULE
@@ -861,6 +862,7 @@ static int __devexit
 wcnss_wlan_remove(struct platform_device *pdev)
 {
 	wcnss_remove_sysfs(&pdev->dev);
+	penv = NULL;
 	return 0;
 }
 
@@ -914,7 +916,6 @@ static void __exit wcnss_wlan_exit(void)
 			pil_put(penv->pil);
 
 
-		kfree(penv);
 		penv = NULL;
 	}
 
