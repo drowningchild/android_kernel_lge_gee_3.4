530bb20dbb62b09c3ff3e13a2445d68beb7172bf
franciscofranco <franciscofranco.1990@gmail.com>
2013-01-29 18:39:04 -0800

    auto_hotplug.c: now we're testing 2 cores always online instead of 4 cores.
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -198,6 +198,7 @@ static void hotplug_online_all_work_fn(struct work_struct *work)
 		if (!cpu_online(cpu)) {
 			cpu_up(cpu);
 			pr_info("auto_hotplug: CPU%d up.\n", cpu);
+			return;
 		}
 	}
 }
